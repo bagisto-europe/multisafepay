05f7dcd4a0df9a28dff3371d7d5a1df08bf4e6e9
Shubham Mehrotra <shubh0896m@gmail.com>
2023-11-05 21:05:05 +0530

    refund transaction generated

--- src/Listeners/MultiSafePayListener.php
+++ src/Listeners/MultiSafePayListener.php
@@ -15,6 +15,10 @@ class MultiSafePayListener
      */
     public function afterRefundGenerated($refund)
     {
+        if ($refund->order->payment->method !== 'multisafepay') {
+            return;
+        }
+
         $apiKey = core()->getConfigData('sales.payment_methods.multisafepay.apikey');
         $isProduction = core()->getConfigData('sales.payment_methods.multisafepay.production');
         $multiSafepaySdk = new \MultiSafepay\Sdk($apiKey, $isProduction);
@@ -26,6 +30,21 @@ class MultiSafePayListener
             $transactionManager = $multiSafepaySdk->getTransactionManager();
             $transaction = $transactionManager->get($orderId);
             $response = $transactionManager->refund($transaction, (new RefundRequest())->addMoney( $refundAmount ) );
+
+            $orderTransactionRepository = app('\Webkul\Sales\Repositories\OrderTransactionRepository');
+            $transaction = $orderTransactionRepository->findWhere([
+                'order_id' => $refund->order_id
+            ])->first();
+            
+            $orderTransactionRepository->create([
+                'transaction_id' => $transaction->transaction_id,
+                'status' => 'refunded',
+                'amount' => $refund->grand_total,
+                'type' => $transaction->type,
+                'payment_method' => $transaction->payment_method,
+                'invoice_id' => $transaction->invoice_id,
+                'order_id' => $transaction->order_id,
+            ]);
             \Log::info("MultiSafePay refund generated for order id: $orderId");
         } catch (\Exception $exception) {
             \Log::info("MultiSafePay exception while generating for order id: $orderId");

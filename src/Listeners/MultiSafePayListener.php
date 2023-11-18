03d598520040f5b0b08dd5674c16077e0e6451b8
Glenn Hermans <glhermans@gmail.com>
2023-11-18 15:17:31 +0100

    added shipping event

--- src/Listeners/MultiSafePayListener.php
+++ src/Listeners/MultiSafePayListener.php
@@ -2,7 +2,11 @@
 
 namespace Bagisto\MultiSafePay\Listeners;
 
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
 use MultiSafepay\ValueObject\Money;
+use MultiSafepay\Api\Transactions\UpdateRequest;
 use MultiSafepay\Api\Transactions\RefundRequest;
 
 class MultiSafePayListener
@@ -29,13 +33,13 @@ class MultiSafePayListener
             $refundAmount = new Money(round($refund->grand_total * 100), $refund->order->base_currency_code);
             $transactionManager = $multiSafepaySdk->getTransactionManager();
             $transaction = $transactionManager->get($orderId);
-            $response = $transactionManager->refund($transaction, (new RefundRequest())->addMoney( $refundAmount ) );
+            $response = $transactionManager->refund($transaction, (new RefundRequest())->addMoney($refundAmount));
 
             $orderTransactionRepository = app('\Webkul\Sales\Repositories\OrderTransactionRepository');
             $transaction = $orderTransactionRepository->findWhere([
                 'order_id' => $refund->order_id
             ])->first();
-            
+
             $orderTransactionRepository->create([
                 'transaction_id' => $transaction->transaction_id,
                 'status' => 'refunded',
@@ -45,12 +49,42 @@ class MultiSafePayListener
                 'invoice_id' => $transaction->invoice_id,
                 'order_id' => $transaction->order_id,
             ]);
-            \Log::info("MultiSafePay refund generated for order id: $orderId");
+            Log::info("MultiSafepay refund generated for order id: $orderId");
         } catch (\Exception $exception) {
-            \Log::info("MultiSafePay exception while generating for order id: $orderId");
-            \Log::info($exception);
+            Log::info("MultiSafepay exception while generating refund for order id: $orderId");
+            Log::info($exception);
         }
 
         return true;
+    }
+
+    /**
+     * After order has been shipped
+     *
+     * @param  \Webkul\Sale\Contracts\Shipment  $shipment
+     * @return void
+     */
+    public function afterOrderShipped($shipment)
+    {
+        if ($shipment->order->payment->method !== 'multisafepay') {
+            return;
+        }
+
+        $apiKey = core()->getConfigData('sales.payment_methods.multisafepay.apikey');
+        $isProduction = core()->getConfigData('sales.payment_methods.multisafepay.production');
+        $multiSafepaySdk = new \MultiSafepay\Sdk($apiKey, $isProduction);
+
+        $orderId = core()->getConfigData('sales.payment_methods.multisafepay.prefix') . $shipment->order_id;
+
+        try {            
+            $updateRequest = new UpdateRequest();
+            $updateRequest->addId($orderId);
+            $updateRequest->addStatus('shipped');
+
+            $multiSafepaySdk->getTransactionManager()->update($orderId, $updateRequest);
+        } catch (\Exception $exception) {
+            Log::error("MultiSafepay exception while trying to update the status to shipped for order id: $orderId");
+            Log::error($exception);
+        }
     }
 }

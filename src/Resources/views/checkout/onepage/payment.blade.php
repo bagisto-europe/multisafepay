51c208665dc1382590480c678c9afd2d54d749da
Shubham Mehrotra <shubh0896m@gmail.com>
2024-01-31 17:44:39 +0530

    minor CSS updates

--- src/Resources/views/checkout/onepage/payment.blade.php
+++ src/Resources/views/checkout/onepage/payment.blade.php
@@ -30,14 +30,14 @@
                         <x-slot:content class="!p-0 mt-8">
                             <div class="flex flex-wrap gap-7">
                                 <div 
-                                    class="relative max-sm:max-w-full max-sm:flex-auto cursor-pointer"
+                                    :class="`relative max-sm:max-w-full max-sm:flex-auto cursor-pointer ${payment.method === 'multisafepay' ? 'w-full' : ''}`"
                                     v-for="(payment, index) in payment_methods"
                                 >
 
                                     {!! view_render_event('bagisto.shop.checkout.payment-method.before') !!}
 
-                                    <div v-if="payment.method === 'multisafepay'" class="multisafepay-container">
-                                        <div v-for="paymentMethod in MultiSafePaymentMethods" class="multisafepay-method">
+                                    <div v-if="payment.method === 'multisafepay'" class="grid grid-cols-4 gap-4">
+                                        <div v-for="paymentMethod in MultiSafePaymentMethods" class="relative">
                                             <input 
                                                 type="radio" 
                                                 name="payment[method]" 
@@ -61,7 +61,7 @@
 
                                             <label 
                                                 :for="'paymentMethod_' + paymentMethod.id"
-                                                class="w-[190px] p-5 block border border-[#E9E9E9] rounded-xl max-sm:w-full cursor-pointer"
+                                                class="p-5 block border border-[#E9E9E9] rounded-xl max-sm:w-full cursor-pointer"
                                             >
 
                                                 {!! view_render_event('bagisto.shop.checkout.onepage.payment-method.image.before') !!}
@@ -214,13 +214,8 @@
     </script>
 
     <style>
-        .multisafepay-container {
-            gap: 10px;
-            display: grid;
-            grid-template-columns: repeat(4, auto);
-        }
-        .multisafepay-method {
-            position: relative;
+        .grid-cols-4 {
+            grid-template-columns: repeat(4,minmax(0,1fr));
         }
     </style>
 @endPushOnce
